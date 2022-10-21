1c15dad24d424511f20bebce1cb5c171ea7d08fd
CodeerBeer1 <42830901+CodeerBeer1@users.noreply.github.com>
2022-10-22 01:21:39 +0200

    Image will now be printed mirrored.

--- main.c
+++ main.c
@@ -6,18 +6,19 @@ int main()
  
     char asciiLarge[] = {" .'`^,:;Il!i><~+fjrxnuvcz0OZmwqpao*#MW&8B@$"};
     char asciiSmall[] = {" .:-=+*#\%@"};
-    int charRate = 255 /  sizeof(asciiSmall);
+    int greyscaleProportion = 255 /  sizeof(asciiSmall);
     int lenalength[2] = { sizeof(lena[0]) / sizeof(int), sizeof(lena[1]) / sizeof(int) };
-
 
     for(int i = 0; i < lenalength[1]; i++)
     {
         for(int o = 0; o < lenalength[0]; o++)
         {
-                int c = lena[i][o] / charRate;
-                printf("%c", asciiSmall[c]);
+            int c = (lena[i][511 - o]) / greyscaleProportion;
+            printf("%c", asciiSmall[c]);
         }
         printf("\n");
     }
+
 return 0;
+
 }
