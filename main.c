2e8c2310f1df31e4cdec2711b4f461e29af031bf
CodeerBeer1 <42830901+CodeerBeer1@users.noreply.github.com>
2023-01-05 16:35:23 +0100

    Data is now provided by the txt file.

--- main.c
+++ main.c
@@ -1,53 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int lena[512][512];
+
+int printLena(int start, int end)
 {
- 
-    int lena[512][512];
 
     char asciiSmall[][2] = { {' ', '0'}, {'.', '1'}, {':', '2'}, {'-', '2'}, {'=', '3'}, {'+', '4'}, {'*', '3'}, {'#', '4'}, {'\%','5'}, {'@', '5'} };
 
     int greyscaleProportion = 255 / ( sizeof(asciiSmall) / sizeof(asciiSmall[0]) );
     int lenalength[2] = { sizeof(lena[0]) / sizeof(int), sizeof(lena[1]) / sizeof(int) };
+    int avg;
 
-    FILE* file = fopen("test.txt", "r");
-
-    //while(!feof(file))
-    //{
-
-        char buffer[12];
-        fgets(buffer, sizeof(buffer),  file);
-
-        char _registers[3][3];
-        int i = 0;
-        for(int a = 0; a < 3; a++)
-        {
-            for (int f = 0; f < 3; i++)
-            {
-                if(buffer[i] != 32)
-                {
-                    _registers[a][f] =+ buffer[i];
-                    
-                    //printf("%c", _registers[a][f]);
-                    f++;
-                }
-                else
-                {
-                    _registers[a][f] =+ ' ';
-                }
-            }
-        }
-       // int s = atoi(buffer);
-
-        printf("%c", _registers[1][1]);
-
-    //}
-
-    /*for(int i = 0; i < lenalength[0]; i++)
+    for(int i = start; i < end; i++)
     {
-        for(int o = 0; o < lenalength[1]; o++)
+        for(int o = start; o < end; o++)
         {
+            avg += lena[i][o];
             int c = (lena[i][(lenalength[0] -1) - o]) / greyscaleProportion;
             char buffer[10];
             sprintf(buffer,  "\033[0;3%cm", asciiSmall[c][1]);
@@ -57,7 +26,41 @@ int main()
             
         }
         printf("\n");
-    }*/
+    }
+
+    return avg / ((end - start) * (end - start));
+}
+
+int main()
+{
+
+    FILE* file = fopen("lena.txt", "r");
+
+    while(!feof(file))
+    {
+        int data[3];
+        int i = 0;
+        char buffer[12];
+        fgets(buffer, sizeof(buffer), file);
+        for (int y = 0; y < 3; y++)
+        {
+            // [4] for the last nul-character.
+            char _register[4];
+            for(int u = 0; u < 3; u++)
+            {
+                _register[u] = buffer[i];
+                i++;
+            }
+            int s = atoi(_register);
+            data[y] = s;
+            i++;
+        }
+        lena[data[0]][data[1]] = data[2];
+    }
+
+int avg = printLena(100, 400);
+
+printf("%d", avg);
 
 return 0;
 
